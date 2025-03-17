f425421d3a6b89f4b91ba0b5323cf039f54540bf
gkaranh09 <codewithkaran09@gmail.com>
2025-03-17 20:55:27 +0530

    Updated2

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,7 @@ import os
 # Ensure the `src` directory is in the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from calculate_sip import calculate_sip  # Correct import path
+from tests.test import calculate_sip  # Correct import path
 
 class TestSIPCalculator(unittest.TestCase):
     def test_sip_calculation(self):
